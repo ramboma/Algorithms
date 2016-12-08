4a56c91aac845b190b4683fc91200b613a07941a
vundler <grawhawk@gmail.com>
2016-12-08 14:54:45 +0800

    v1.1

--- Algorithms-1/Week4/8puzzle/Board.java
+++ Algorithms-1/Week4/8puzzle/Board.java
@@ -1,195 +1,185 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.Stack;
-/**
- * Created by lu on 4/5/14.
- * a NxN slider game tiles
- */
-public class Board {
-    private final int N;            // tiles size
-    private final int[][] tiles;    // current tiles
 
-    /**
-     * construct a tiles from an N-by-N array of tiles
-     */
-    public Board(int[][] blocks) {
-        N = blocks.length;
-        tiles = deepCopy(blocks, N);
+public class Board
+{
+    private int[][] blocks;
+    private int demain;
+    private int local;
+    public Board(int[][] blocks)// construct a board from an n-by-n array of blocks  (where blocks[i][j] = block in row i, column j)
+    {
+        this.blocks=blocks;
+        demain=blocks.length;
+        local=getLocal(); 
     }
-
-    // helper for deep copy a 2D array
-    private int[][] deepCopy(int[][] y, int M) {
-        int[][] out = new int[M][M];
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < M; j++) {
-                out[i][j] = y[i][j];
+    private int getLocal()
+    {
+        for(int row=0;row<demain;row++)
+            for(int col=0;col<demain;col++)
+                if(blocks[row][col]==0)
+                    return row*demain+col;
+        throw new java.lang.NullPointerException();
+    }
+    public int dimension()                 // board dimension n
+    {
+        return demain;
+    }
+    public int hamming()// number of blocks out of place
+    {
+        int hammingCount=0;
+        for(int row=0;row<demain;row++)
+            for(int col=0;col<demain;col++)
+            {
+                if(blocks[row][col]!=row*demain+col+1)
+                    if(row==demain-1&&col==demain-1)
+                    {}
+                    else
+                    {
+                        hammingCount++;
+                    }
             }
-        }
-        return out;
+        return hammingCount;
     }
-
-    /**
-     * tiles dimension N
-     */
-    public int dimension() {
-        return N;
-    }
-
-    /**
-     * number of tiles out of place
-     */
-    public int hamming() {
-        int numOfDiffBlocks = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (tiles[i][j] != j + i * N  + 1) {
-                    numOfDiffBlocks++;
+    public int manhattan()// sum of Manhattan distances between blocks and goal
+    {
+        int manhattanCount=0;
+        for(int row=0;row<demain;row++)
+            for(int col=0;col<demain;col++)
+            {
+                if(blocks[row][col]!=0)
+                {
+                    int distinct=getDistince(row,col);
+                    manhattanCount+=distinct;
                 }
             }
-        }
-        return --numOfDiffBlocks; // exclude the blank block
+        return manhattanCount;
     }
-
-    // helper to compute the Manhattan distance for a single block
-    private int singleMan(int i, int j) {
-        int goalI = tiles[i][j] / N;
-        int goalJ = tiles[i][j] % N;
-        if (goalJ == 0) {
-            goalI -= 1;
-            goalJ = N - 1;
-        } else {
-            goalJ -= 1;
-        }
-        return Math.abs(i - goalI) + Math.abs(j - goalJ);
+    private int getDistince(int row,int col)
+    {
+        int curValue=blocks[row][col];
+        int shouldRow=(curValue-1)/demain;
+        int shouldCol=(curValue-1)%demain;
+        int rowDis=Math.abs(shouldRow-row);
+        int colDis=Math.abs(shouldCol-col);
+        return rowDis+colDis;
     }
-
-    /**
-     * sum of Manhattan distance between tiles and goal
-     */
-    public int manhattan() {
-        int manDist = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (tiles[i][j] != j + i * N  + 1 && tiles[i][j] != 0) {
-                    manDist += singleMan(i, j);
-                }
+    public boolean isGoal()                // is this board the goal board?
+    {
+        return manhattan()==0;
+    }
+    public Board twin()                    // a board that is obtained by exchanging any pair of blocks
+    {
+       int[][] aTwin=deepclone(blocks,demain);
+       if(aTwin[0][0]*aTwin[0][1]==0)
+       {
+           exchange(aTwin,1,0,1,1);
+       }
+       else
+       {
+           exchange(aTwin,0,0,0,1);
+       }
+       return new Board(aTwin);
+    }
+    public boolean equals(Object y)        // does this board equal y?
+    {
+        if(y==this) return true;
+        if(!(y instanceof Board)) return false;
+        Board that=(Board)y;
+        return (this.demain==that.demain)&&(Arrays.deepEquals(this.blocks,that.blocks));
+        //不能用manhattan值判断相等,不准
+        //Board yBoard=(Board)y;
+        //if(manhattan()==yBoard.manhattan())
+        //    return true;
+        //return false;
+    }
+    public Iterable<Board> neighbors()     // all neighboring boards
+    {
+        ArrayList<Board> neighborList=new ArrayList<Board>();
+        int xLocal=local/demain;
+        int yLocal=local%demain;
+        if(xLocal>0)
+        {
+            int[][] upBlock=deepclone(blocks,demain);
+            exchange(upBlock,xLocal,yLocal,xLocal-1,yLocal);
+            Board upNeighbor=new Board(upBlock);
+            neighborList.add(upNeighbor); 
+        }
+        if(xLocal<demain-1)
+        {
+            int[][] downBlock=deepclone(blocks,demain);
+            exchange(downBlock,xLocal,yLocal,xLocal+1,yLocal);
+            Board downNeighbor=new Board(downBlock);
+           neighborList.add(downNeighbor); 
+        }
+        if(yLocal>0)
+        {
+            int[][] leftBlock=deepclone(blocks,demain);
+            exchange(leftBlock,xLocal,yLocal,xLocal,yLocal-1);
+            Board leftNeighbor=new Board(leftBlock);
+           neighborList.add(leftNeighbor); 
+        }
+        if(yLocal<demain-1)
+        {
+            int[][] rightBlock=deepclone(blocks,demain);
+            exchange(rightBlock,xLocal,yLocal,xLocal,yLocal+1);
+            Board rightNeighbor=new Board(rightBlock);
+           neighborList.add(rightNeighbor); 
+        }
+        return  neighborList;
+    }
+    private void exchange(int[][] Neighbor,int xLocal,int yLocal,int x1Local, int y1Local)
+    {
+      int temp=Neighbor[xLocal][yLocal];
+      Neighbor[xLocal][yLocal]=Neighbor[x1Local][y1Local];
+      Neighbor[x1Local][y1Local]=temp;
+    }
+    private int[][] deepclone(int[][]b,int d)
+    {
+        int[][] copy=new int[d][d];
+        for(int row=0;row<d;row++)
+            for(int col=0;col<d;col++)
+            {
+                copy[row][col]=b[row][col];
             }
-        }
-        return manDist;
+        return copy;
     }
-
-    /**
-     * is this tiles the goal tiles?
-     */
-    public boolean isGoal() {
-        return manhattan() == 0;
-    }
-
-    // helper for swapping two elements in a 2D array
-    private void swapBlocks(int[][] x, int i, int j, int m, int n) {
-        int swap;
-        swap = x[i][j];
-        x[i][j] = x[m][n];
-        x[m][n] = swap;
-    }
-
-    /**
-     * a tiles obtained by exchanging two adjacent tiles in the same row
-     */
-    public Board twin() {
-        int[][] aTwin = deepCopy(tiles, N);
-        if (aTwin[0][0] * aTwin[0][1] == 0) {
-            swapBlocks(aTwin, 1, 0, 1, 1);
-        } else {
-            swapBlocks(aTwin, 0, 0, 0, 1);
-        }
-        return new Board(aTwin);
-    }
-
-    /**
-     * does this tiles equals y?
-     * @param y another tiles
-     */
-    public boolean equals(Object y) {
-        //check for reference equality
-        if (y == this) return true;
-
-        //use instanceof instead of getClass here for two reasons
-        //1. if need be, it can match any super type, and not just one class;
-        //2. it renders an explicit check for "y == null" redundant, since
-        //it does the check for null already - "null instanceof [type]" always
-        //returns false. (See Effective Java by Joshua Bloch.)
-        if (!(y instanceof Board)) return false;
-
-        //let the compiler know y is a "Board" instead of an "Object"
-        Board that = (Board) y;
-        return (this.N == that.N) && (Arrays.deepEquals(this.tiles, that.tiles));
-
-    }
-
-    // helper method to push neighbor board onto a stack
-    private void pushBoardOntoStack(Stack<Board> s, int x, int y, int m, int n) {
-        int[][] neighborTiles = deepCopy(tiles, N);
-        swapBlocks(neighborTiles, x, y, m, n);
-        s.push(new Board(neighborTiles));
-    }
-
-    /**
-     * all neighboring boards
-     */
-    public Iterable<Board> neighbors() {
-        Stack<Board> boards = new Stack<Board>();
-        boolean blankFound = false;
-        for (int i = 0; i < N && !blankFound; i++) {
-            for (int j = 0; j < N; j++) {
-                if (tiles[i][j] == 0) {
-                    if (i > 0) pushBoardOntoStack(boards, i - 1, j, i, j);
-                    if (j > 0) pushBoardOntoStack(boards, i, j - 1, i, j);
-                    if (i < N - 1) pushBoardOntoStack(boards, i + 1, j, i, j);
-                    if (j < N - 1) pushBoardOntoStack(boards, i, j + 1, i, j);
-                    blankFound = true;
-                    break;
-                }
+    public String toString()               // string representation of this board (in the output format specified below)
+    {
+        StringBuffer sb=new StringBuffer();
+        sb.append(demain+"\r\n");
+        for(int row=0;row<demain;row++)
+        {
+            for(int col=0;col<demain;col++)
+            {
+                sb.append(String.format("%2d ",blocks[row][col]));
             }
+            sb.append("\r\n");
         }
-        return boards;
+        return sb.toString();
     }
 
-    /**
-     * string representation of the tiles
-     */
-    public String toString() {
-        StringBuffer s = new StringBuffer();
-        s.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tiles[i][j]));
-            }
-            s.append("\n");
-        }
-        return s.toString();
-    }
-
-
-
-
-    public static void main(String[] args) {
-//        int[][] tiles = {{1, 3, 7}, {0, 2, 8}, {4, 6, 5}};
-//        int[][] tiles = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
-        int[][] blocks = {{8, 1, 3}, {4, 2, 0}, {7, 6, 5}};
-//        int[][] tiles = {{1, 0}, {2, 3}};
-        Board a = new Board(blocks);
+    public static void main(String[] args) // unit tests (not graded)
+    {
+        int[][] initBlock={
+            {0,1,3},
+            {4,2,5},
+            {7,8,6}
+        };
+        Board a=new Board(initBlock);
         StdOut.println(a.dimension());
         StdOut.println(a.hamming());
         StdOut.println(a.manhattan());
         StdOut.println(a);
         StdOut.println(a.twin());
-        StdOut.println(a);
-        for (Board board: a.neighbors()) {
+        for(Board board:a.neighbors())
+        {
             StdOut.println(board);
         }
+
     }
+
 }
